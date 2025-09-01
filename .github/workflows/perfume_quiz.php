c42c8dde77a8395710752a98ea042ea8d9bd01ab
ThisismyCodebasenum2 <atrekhobseo2@gmail.com>
2025-09-01 18:03:35 +0330

    Create perfume_quiz.php

---
+++ .github/workflows/perfume_quiz.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Perfume Quiz
+ * Description: آزمون شخصیت عطر با مدیریت تست/سوال/پاسخ، تفسیر نتایج، ایمپورت/اکسپورت، اتصال ووکامرس و پروفایل کاربر.
+ * Version: 0.9.1
+ * Requires at least: 5.8
+ * Requires PHP: 7.4
+ * Author: You
+ * Text Domain: perfume-quiz
+ */
